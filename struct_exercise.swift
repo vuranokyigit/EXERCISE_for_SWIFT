fd7b843bc1fcfada1c57c54839eb88187b440d38
VuranokYigit <fyvuranokk@gmail.com>
2023-02-15 14:46:55 +0300

    Update struct_exercise.swift

--- struct_exercise.swift
+++ struct_exercise.swift
@@ -19,15 +19,12 @@ struct User{
     richard.logStatus()
     
 
-
-
-    // Diagnostic code - do not change this code
     print("\nDiagnostic code (i.e., Challenge Hint):")
     var musk = User(name: "Elon", email: "elon@tesla.com", followers: 2001, isActive: true)
     musk.logStatus()
     print("Contacting \(musk.name) on \(musk.email!) ...")
     print("\(musk.name) has \(musk.followers) followers")
-    // sometime later
+  
     musk.isActive = false
     musk.logStatus()
     
